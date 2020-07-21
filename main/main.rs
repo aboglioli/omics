c3c1c06a9cfec45f9f1fdf749886698fc76acaac
Alan Boglioli <alan.boglioli@gmail.com>
2020-07-21 18:26:48 -0300

    example/main: testing Hyper to make API REST

--- main/main.rs
+++ main/main.rs
@@ -1,6 +1,22 @@
-use std::error::Error;
+use std::{convert::Infallible, net::SocketAddr};
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
 
-fn main() -> Result<(), Box<dyn Error>> {
-    println!("¡Omics!");
-    Ok(())
+async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("¡Omics!".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
