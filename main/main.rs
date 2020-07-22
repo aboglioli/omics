766938a09b8fac5115ed81aaae68d96a4792c6c5
Alan Boglioli <alan.boglioli@gmail.com>
2020-07-21 23:57:57 -0300

    fix/main: solve warnings

--- main/main.rs
+++ main/main.rs
@@ -1,22 +1,21 @@
-use std::{convert::Infallible, net::SocketAddr};
-use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::{convert::Infallible, net::SocketAddr};
 
-async fn handle(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new("¡Omics!".into()))
 }
 
 #[tokio::main]
 async fn main() {
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let port = 3000;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
-    let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(handle))
-    });
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
-    println!("Listening on port {}", 3000);
+    println!("Listening on port {}", port);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
