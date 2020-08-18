15f2074a3ce8d996d776cd41521824475df8c9dd
Momfus <momfus@outlook.com>
2020-08-18 16:41:17 -0300

    Añadido observable para saber si esta logueado el usuario

--- web/src/app/domain/services/auth.ts
+++ web/src/app/domain/services/auth.ts
@@ -1,22 +1,46 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable()
 export class AuthService {
+
   private authToken: string;
 
+  public accessUser = new Subject<boolean>();
+  public accessUser$ = this.accessUser.asObservable();
+
   constructor() {
+
     const authToken = localStorage.getItem('auth_token');
     if (authToken) {
       this.authToken = authToken;
     }
+
+  }
+
+  private updateStateSession( newValue: boolean ): void {
+
+    this.accessUser.next( newValue );
+
   }
 
   public setToken(authToken: string, idUser: string): void {
+
     this.authToken = authToken;
     localStorage.setItem('auth_token', this.authToken);
     localStorage.setItem('id_user', idUser);
+
+    this.updateStateSession( true );
+
+  }
+
+  public logout(): void {
+
+    this.updateStateSession(false);
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('id_user');
+
   }
 
   public getToken(): string {
