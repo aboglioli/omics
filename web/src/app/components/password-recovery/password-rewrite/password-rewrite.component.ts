8c07810acf99cfc3da93dc1d17f634fbd016e5f6
Momfus <momfus@outlook.com>
2020-08-31 21:27:42 -0300

    Agregado la plantilla para recuperar la contraseñña

---
+++ web/src/app/components/password-recovery/password-rewrite/password-rewrite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-rewrite',
+  templateUrl: './password-rewrite.component.html',
+  styleUrls: ['./password-rewrite.component.scss']
+})
+export class PasswordRewriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- web/src/app/shared/nav-bar/nav-bar.component.ts
+++ web/src/app/shared/nav-bar/nav-bar.component.ts
@@ -22,7 +22,7 @@ export class NavBarComponent implements OnInit {
 
   constructor(  private router: Router,
                 private authService: AuthService,
-                public dialog: MatDialog ) {
+                private dialog: MatDialog ) {
 
 
     this.subscribeAuthService();
