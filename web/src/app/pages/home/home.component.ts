fcda1a831094c13cc24be835c2e9daf309005e46
Momfus <momfus@outlook.com>
2020-09-01 16:32:35 -0300

    Recovery password works

--- web/src/app/pages/home/home.component.ts
+++ web/src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { PasswordRewriteComponent } from '../../components/password-recovery/password-rewrite/password-rewrite.component';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  private paramsToUse: any;
+
+  constructor(  private route: ActivatedRoute,
+                private dialog: MatDialog ) { }
 
   ngOnInit(): void {
+
+    if ( this.isRecoverPasswordNeeded() ) {
+
+      this.showRecoverPasswrodModal();
+
+    }
+
+  }
+
+  private isRecoverPasswordNeeded(): boolean {
+
+    let isNeeded: boolean;
+
+    this.route.params.subscribe( (params: any) => {
+
+      isNeeded = (params.id) ? true : false;
+      this.paramsToUse = params;
+
+    });
+
+    return isNeeded;
+
+  }
+
+  private showRecoverPasswrodModal(): void {
+
+    const dialogRef = this.dialog.open(PasswordRewriteComponent, {
+      data: {
+              userId:  this.paramsToUse.id,
+              temporalPass: this.paramsToUse.temporal_password
+            },
+      panelClass: 'no-padding-dialog'
+    });
+
   }
 
 }
