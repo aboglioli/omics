dbde9b8a022ad08690122a90e08959396025d7dd
Momfus <momfus@outlook.com>
2020-09-13 19:38:50 -0300

    Refactorizaddo código de dasboard gestionar contenido

--- web/src/app/pages/dashboard-gestion-contratos-publicaciones/dashboard-gestion-contratos-publicaciones.component.ts
+++ web/src/app/pages/dashboard-gestion-contratos-publicaciones/dashboard-gestion-contratos-publicaciones.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { IPublication } from '../../domain/models';
 import { PublicationService } from '../../domain/services/publication.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-dashboard-gestion-contratos-publicaciones',
@@ -9,51 +10,65 @@ import { PublicationService } from '../../domain/services/publication.service';
   styleUrls: ['./dashboard-gestion-contratos-publicaciones.component.scss']
 })
 export class DashboardGestionContratosPublicacionesComponent implements OnInit {
-  public publications: IPublication[];
+  public publicationList: IPublication[];
 
   constructor(
     private publicationService: PublicationService,
+    private spinnerService: NgxSpinnerService,
   ) { }
 
   ngOnInit(): void {
+
+
+    this.getAllPublication();
+
+  }
+
+  private getAllPublication(): void {
+
+    this.spinnerService.show();
+
     this.publicationService.search({ status: 'waiting-approval' }, 'author,category').subscribe(
-      (res) => {
-        this.publications = res.publications;
-        console.log(this.publications);
+      (res: any) => {
+
+        this.publicationList = res.publications;
+        console.log(this.publicationList);
+
+        this.spinnerService.hide();
+
       },
-      (err) => {
-        console.log(err);
+      (err: Error) => {
+
+        console.error(err);
+        this.spinnerService.hide();
+
       },
     );
+
   }
 
   approve(publication: IPublication): void {
-    this.publicationService.approve(
-      publication.id,
-      {
-        comment: 'comment...' // TODO: take from input
-      }
-    ).subscribe(
-      res => {
+
+    const commentPublication = 'comment...';  // TODO: sacar de un input de un popup
+    this.publicationService.approve( publication.id,  { comment: commentPublication }).subscribe(
+      (res: any) => {
         console.log(res);
       },
-      err => {
+      (err: Error) => {
         console.log(err);
       }
     );
+
   }
 
   reject(publication: IPublication): void {
-    this.publicationService.reject(
-      publication.id,
-      {
-        comment: 'comment...' // TODO: take from input
-      }
-    ).subscribe(
-      res => {
+
+    const commentPublication = 'comment...'; // TODO: sacar de un input de un popup
+    this.publicationService.reject(publication.id, { comment: commentPublication }).subscribe(
+      (res: any) => {
         console.log(res);
       },
-      err => {
+      (err: Error) => {
         console.log(err);
       }
     );
