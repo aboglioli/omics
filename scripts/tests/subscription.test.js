77b22049cc70118448b7ac231e7c16d18bbde2b7
Alan Boglioli <alan.boglioli@gmail.com>
2020-10-08 01:21:00 -0300

    add/scripts: example of using knex to populate db before running test

--- scripts/tests/subscription.test.js
+++ scripts/tests/subscription.test.js
@@ -1,19 +1,37 @@
 const { v4: uuidv4 } = require('uuid');
 
-const { knex } = require('../core/db');
+const { knex, password } = require('../core/db');
+const { req } = require('../core/request');
 
 describe('Subscription', () => {
-  before(async () => {
+  const user_id = uuidv4();
+
+  beforeAll(async () => {
     await knex('users')
       .insert({
-        id: uuidv4(),
+        id: user_id,
         provider: 'local',
         username: 'user-1',
         email: 'user-1@omics.com',
+        password,
+        name: 'Name',
+        lastname: 'Lastname',
+        birthdate: '1994-05-01T14:30:00Z',
+        gender: 'male',
+        role_id: 'user',
+        created_at: new Date(),
       });
+
+    await req.login('admin-1', 'P@asswd!');
   });
 
-  after(async () => {
+  afterAll(async () => {
     await knex('users').where('username', 'user-1').del();
+    await knex.destroy();
+  });
+
+  test('example', async () => {
+    let { data: user } = await req.get(`/users/${user_id}`);
+    expect(user.username).toBe('user-1');
   });
 });
