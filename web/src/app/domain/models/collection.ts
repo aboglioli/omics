513b103b268c2e33ec6449b9ab5a1e02a5bf40db
Alan Boglioli <alan.boglioli@gmail.com>
2020-10-09 00:43:42 -0300

    feature/web: add 'publications' property to IAuthor and ICollection

--- web/src/app/domain/models/collection.ts
+++ web/src/app/domain/models/collection.ts
@@ -9,4 +9,8 @@ export interface ICollection {
   category_id?: string;
   category?: ICategory;
   tags: string[];
+  cover: string;
+  publications: number;
+  created_at: string;
+  updated_at?: string;
 }
