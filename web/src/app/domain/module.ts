fada1a77b387084b3335d237a0f6a5a57c22cc22
Alan Boglioli <alan.boglioli@gmail.com>
2020-10-12 01:36:41 -0300

    feature/web: add new services to DomainModule

--- web/src/app/domain/module.ts
+++ web/src/app/domain/module.ts
@@ -7,11 +7,14 @@ import { AuthService, AuthInterceptor } from './services/auth.service';
 import { CategoryService } from './services/category.service';
 import { CollectionService } from './services/collection.service';
 import { ConfigService } from './services/config.service';
+import { ContractService } from './services/contract.service';
 import { FileService } from './services/file.service';
 import { IdentityService } from './services/identity.service';
 import { NotificationService } from './services/notification.service';
+import { PlanService } from './services/plan.service';
 import { PublicationService } from './services/publication.service';
 import { ReaderService } from './services/reader.service';
+import { SubscriptionService } from './services/subscription.service';
 
 @NgModule({
   imports: [
@@ -29,6 +32,9 @@ import { ReaderService } from './services/reader.service';
     NotificationService,
     PublicationService,
     ReaderService,
+    PlanService,
+    ContractService,
+    SubscriptionService,
 
     // Auth interceptor
     {
