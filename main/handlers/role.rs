c21492e61e365a6075ed6e7a398b85ed7a7fe7c7
Alan Boglioli <alan.boglioli@gmail.com>
2020-11-20 01:32:39 -0300

    feature/main: add role API handlers

--- main/handlers/role.rs
+++ main/handlers/role.rs
@@ -1,7 +1,10 @@
-use actix_web::{get, post, web, HttpRequest, HttpResponse, Responder};
+use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse, Responder};
 
 use common::request::{IncludeParams, PaginationParams};
-use identity::application::role::{Create, CreateCommand, GetAll, GetById};
+use identity::application::role::{
+    Create, CreateCommand, Delete, GetAll, GetById, GetPermissions, MakeDefault, Update,
+    UpdateCommand,
+};
 use identity::application::user::{Search as SearchUser, SearchCommand as SearchUserCommand};
 
 use crate::authorization::auth;
@@ -76,11 +79,69 @@ async fn create(
         .map_err(PublicError::from)
 }
 
+#[put("/{role_id}")]
+async fn update(
+    req: HttpRequest,
+    path: web::Path<String>,
+    cmd: web::Json<UpdateCommand>,
+    c: web::Data<MainContainer>,
+) -> impl Responder {
+    let user_id_and_role = auth(&req, &c).await?;
+
+    Update::new(c.identity.permission_repo(), c.identity.role_repo())
+        .exec(user_id_and_role, path.into_inner(), cmd.into_inner())
+        .await
+        .map(|res| HttpResponse::Ok().json(res))
+        .map_err(PublicError::from)
+}
+
+#[delete("/{role_id}")]
+async fn delete(
+    req: HttpRequest,
+    path: web::Path<String>,
+    c: web::Data<MainContainer>,
+) -> impl Responder {
+    let user_id_and_role = auth(&req, &c).await?;
+
+    Delete::new(c.identity.role_repo(), c.identity.user_repo())
+        .exec(user_id_and_role, path.into_inner())
+        .await
+        .map(|res| HttpResponse::Ok().json(res))
+        .map_err(PublicError::from)
+}
+
+#[put("/{role_id}/default")]
+async fn make_default(
+    req: HttpRequest,
+    path: web::Path<String>,
+    c: web::Data<MainContainer>,
+) -> impl Responder {
+    let user_id_and_role = auth(&req, &c).await?;
+
+    MakeDefault::new(c.identity.role_repo())
+        .exec(user_id_and_role, path.into_inner())
+        .await
+        .map(|res| HttpResponse::Ok().json(res))
+        .map_err(PublicError::from)
+}
+
+#[get("/")]
+async fn get_permissions(req: HttpRequest, c: web::Data<MainContainer>) -> impl Responder {
+    let user_id_and_role = auth(&req, &c).await?;
+
+    GetPermissions::new(c.identity.permission_repo())
+        .exec(user_id_and_role)
+        .await
+        .map(|res| HttpResponse::Ok().json(res))
+        .map_err(PublicError::from)
+}
+
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/roles")
             .service(get_all)
             .service(get_by_id)
             .service(get_users),
-    );
+    )
+    .service(web::scope("/permissions").service(get_permissions));
 }
