9d3ddd72e40bdc7c871d39ca8ac18ccb9d77ca1a
Momfus <momfus@outlook.com>
2020-11-20 22:31:46 -0300

    Añadido checkbox para setear rol predeterminado

--- web/src/app/components/dashboard/dashboard-roles/roles-manager/roles-manager-list/roles-manager-list.component.ts
+++ web/src/app/components/dashboard/dashboard-roles/roles-manager/roles-manager-list/roles-manager-list.component.ts
@@ -3,6 +3,20 @@ import { MatDialog } from '@angular/material/dialog';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 import { RolesManagerEditComponent } from '../roles-manager-edit/roles-manager-edit.component';
 
+// TODO: Borrar luego de aplicar servicio
+const MOCKUP_DATA: any[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-roles-manager-list',
   templateUrl: './roles-manager-list.component.html',
@@ -10,8 +24,12 @@ import { RolesManagerEditComponent } from '../roles-manager-edit/roles-manager-e
 })
 export class RolesManagerListComponent implements OnInit {
 
+
   // Font Awseome icons
   public faAdd = faPlusCircle;
+
+  // Información tablas
+  tableRoleData: any[] = MOCKUP_DATA;
 
   constructor(
     private dialog: MatDialog,
